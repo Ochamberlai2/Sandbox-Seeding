d0f58b0c1f860f4f7f8f874e8b617b809a5c9f30
Oliver Chamberlain <ochamberlai2@gmail.com>
2023-10-23 22:38:37 +0100

    Beginning of lookup value saving - frontend

--- force-app/main/default/lwc/sandboxSeeding/sandboxSeeding.js
+++ force-app/main/default/lwc/sandboxSeeding/sandboxSeeding.js
@@ -1,6 +1,5 @@
 import { LightningElement, wire, track } from "lwc";
 import getObjects from "@salesforce/apex/SandboxSeedingUtil.getObjects";
-import getFields from "@salesforce/apex/SandboxSeedingUtil.getFields";
 import seedSandbox from "@salesforce/apex/SandboxSeedingUtil.seedSandbox";
 import insertSeedingTemplate from "@salesforce/apex/CustomMetadataUtil.insertSeedingTemplate";
 
@@ -23,44 +22,13 @@ export default class SandboxSeeding extends LightningElement {
   objects;
   objectsOptions;
   selectedObject;
-  @wire(getFields, { selectedObject: "$selectedObject" })
-  fieldsWire({ data, error }) {
-    this.fields = [];
-    if (data) {
-      data.forEach((field) => {
-        this.fields.push({
-          key: field
-        });
-      });
-    }
-  }
+
   @track
   fields;
-  templateName;
   numberOfRecords;
-  typeOptions = [
-    { label: "Company Name", value: "CompanyName" },
-    { label: "First Name", value: "FirstName" },
-    { label: "Last Name", value: "LastName" },
-    { label: "Date", value: "Date" },
-    { label: "Number", value: "Number" },
-    { label: "Text Value", value: "TextValue" },
-    { label: "Specific Value", value: "SpecificValue" },
-    { label: "--None--", value: "" }
-  ];
 
   handleObjectSelect(event) {
     this.selectedObject = event.target.value;
-  }
-
-  handleTypeSelect(event) {
-    const field = this.fields.find(
-      (fieldObject) => event.target.dataset.key === fieldObject.key
-    );
-    if (field) {
-      field.type = event.target.value;
-      field.isSpecific = event.target.value === "SpecificValue";
-    }
   }
   handleTemplateNameChange(event) {
     this.templateName = event.target.value;
@@ -68,14 +36,10 @@ export default class SandboxSeeding extends LightningElement {
   handleNumberRecordsChange(event) {
     this.numberOfRecords = event.target.value;
   }
-  handleSpecificValueChange(event) {
-    const field = this.fields.find(
-      (fieldObject) => event.target.dataset.key === fieldObject.key
-    );
-    if (field) {
-      field.specificValue = event.target.value;
-    }
+  handleFieldChange(event) {
+    this.fields = event.detail;
   }
+
   createObjectTemplate() {
     const populatedFields = this.fields.filter(
       (field) => field.type !== undefined
