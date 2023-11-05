4d8b021f89f73e88f775d2d36a2fb5dcd1b6a04a
Oliver Chamberlain <ochamberlai2@gmail.com>
2023-11-05 18:31:07 +0000

    Started creating template list

---
+++ force-app/main/default/lwc/sandboxSeedingTemplateList/sandboxSeedingTemplateList.js
@@ -0,0 +1,13 @@
+import { LightningElement, wire } from "lwc";
+import getSeedingTemplates from "@salesforce/apex/SandboxSeedingUtil.getSeedingTemplates";
+export default class SandboxSeedingTemplateList extends LightningElement {
+  seedingTemplates;
+  @wire(getSeedingTemplates)
+  getSeedingTemplates({ data, error }) {
+    if (error) {
+      console.error(error);
+    } else if (data) {
+      this.seedingTemplates = data;
+    }
+  }
+}
